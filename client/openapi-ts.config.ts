ca6987132501466112c89312423030c8e1f0c7c8
Pavel Dolbik <72512676+Mel6chior@users.noreply.github.com>
2025-11-01 12:33:18 +0300

    fix: delete previous client to setup app correctly

--- client/openapi-ts.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "@hey-api/openapi-ts";
-import { config } from "dotenv";
-
-config();
-
-export default defineConfig({
-  input: <string>process.env.API_URL,
-  output: "./app/generated/api",
-})
