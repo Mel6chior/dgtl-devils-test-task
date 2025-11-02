5d40144e7ca541705407ae39ce62f92e57e60fe3
Pavel Dolbik <72512676+Mel6chior@users.noreply.github.com>
2025-11-02 10:06:09 +0300

    feat: added summary to auth routes

--- server/src/core/features/auth/auth.controller.ts
+++ server/src/core/features/auth/auth.controller.ts
@@ -2,17 +2,20 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from './dto/login-user.dto';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
+  @ApiOperation({ summary: 'Register new user'})
   async register(@Body() dto: RegisterUserDto) {
     return await this.authService.register(dto);
   }
 
   @Post('login')
+  @ApiOperation({ summary: 'Authenticate user'})
   async login(@Body() dto: LoginUserDto) {
     return await this.authService.login(dto);
   }
